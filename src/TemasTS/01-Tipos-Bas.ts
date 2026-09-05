a67e52db9fa508d3d076663f37a5b8a5db4fc852
IEVN1003-18002441 <18002441@alumnos.utleon.edu.mx>
2026-09-04 20:20:58 -0600

    Tipos de datos 04/09/2026

---
+++ src/TemasTS/01-Tipos-Bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+
+var num1= 11.22;
+let num2 =23.52;
+
+
+let nombre: string = "Pedro";
+let num3: number;
+let activo: boolean;
+let resultado: number = num1+num2;
+
+console.log("Hola "+nombre+" La operación de "+num1+" + "+num2+" es: "+ resultado);
+
+console.log(nombre);
+nombre = 22;
+console.log(nombre);
