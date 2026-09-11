f24f5f51412b08c07ae2b87d5e22640e5151a7e4
IEVN1003-18002441 <18002441@alumnos.utleon.edu.mx>
2026-09-10 19:20:55 -0600

    TS Final 10/09/26

---
+++ src/TemasTS/07-clases.ts
@@ -0,0 +1,20 @@
+class Persona {
+    nombre: string
+    edad: number
+
+
+constructor (nombre:string, edad:number){
+    this.nombre=nombre
+    this.edad=edad
+}
+
+imprimir(){
+    console.log(`hola me llamo ${this.nombre} y tengo ${this.edad} años`)
+
+
+}
+}
+
+let persona:Persona 
+persona= new Persona("Mario", 22)
+persona.imprimir()
